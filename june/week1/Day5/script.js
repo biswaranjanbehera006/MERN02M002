d463751d0cffaf46edfa6bf577bea2cce9c0a9e3
biswaranjanbehera006 <biswaranjanbehera006@gmail.com>
2025-06-06 13:10:36 +0530

    javascript learning

---
+++ june/week1/Day5/script.js
@@ -0,0 +1,234 @@
+// console.log('Welcome to qlith Inovation pvt ltd'); 
+
+// // Varibale Declaration
+
+// let a=1;
+// let A=2;
+
+// console.log(a);
+// console.log(A);
+// let _="Qlith";
+
+// console.log(_);
+
+
+// let $="Qlith inovation";
+// console.log($);
+
+// let $name="inovation";
+// console.log($name);
+
+// //
+
+// let nameOfStudent="Biswa Ranjan Behera"; //camel case
+// console.log(nameOfStudent);
+
+
+// let EmployeeName="biswa"; //pascal case
+
+
+// let c=20;
+// console.log(c);
+// console.log(typeof c);
+
+// let name="Krish";
+// console.log(name);
+// console.log(typeof name);
+// console.log(name ,"type is",typeof name);
+
+
+// let ispass=true;
+// console.log(typeof ispass);
+
+
+// let emp=undefined;
+// console.log(emp);
+
+// let nulvalue=null;
+// console.log(null);
+
+// let m;
+// console.log(m);
+
+// let f=30;
+// console.log(f);
+// console.log(typeof typeof f);
+
+
+// let e=30.4;
+// console.log(typeof e);
+
+
+// //Global declaration of the variable
+// var fullname="Qlith Inovation";
+// console.log(fullname);
+
+// function getName(){
+//     console.log(fullname);
+// }
+
+// getName();
+
+// //normal block
+// //This my blockl level declaration of varibale
+// {
+//     var a=40;
+//     console.log(a);
+    
+// }
+
+// console.log("local access",a);
+
+// function getA()
+// {
+//     console.log(a);
+    
+// }
+
+// getA();
+
+
+// function variableDeclare()
+// {
+//     c=10;
+//     console.log(c);
+    
+// }
+
+// console.log(c);//can't access because var is declared in the function
+
+
+//This is the let varibale declaration
+
+ //global declartion 
+// let name="inovation";
+
+// console.log(name);
+// {
+//     console.log(name);
+    
+// }
+
+// function getname()
+// {
+//     console.log(name);
+    
+// }
+// getname();
+
+
+// //Block level Declartion
+
+// {
+//     let a=30;
+//     console.log(a);
+    
+// }
+
+// // console.log(a);
+
+// function letacess()
+// {
+//     // console.log(name);
+//     console.log(a);
+      
+// }
+
+// letacess();
+
+
+// This is the const varibale declaration
+
+// const name="xyz";
+// console.log(name);
+
+// {
+//     console.log(name);
+    
+// }
+
+// function const_access(){
+//     console.log(name);
+    
+// }
+
+// const_access();
+
+// {
+//     const a=70;
+//     console.log(a);
+    
+// }
+
+// console.log(a);
+
+
+
+
+// var a=60;
+// console.log(a);
+
+
+// var a=70;
+// console.log(a);
+
+// a="y"
+// console.log(a);
+
+
+//we can't redeclare the varibale using let
+// let b=60;
+// console.log(a);
+
+// let b=70;
+// console.log(a);
+
+//reinsilaztion possible 
+// let a=50;
+// console.log(a);
+// a=90;
+// console.log(a);
+
+
+//const is not allowed redclartion and insilaztaion
+// const s="String";
+// console.log(s);
+// s="rohan"
+// console.log(s);
+
+// ***********************************************************************
+//Differnece between var , let ,const
+
+// *********var******
+// redclartion and reinsilazation possible
+
+// ***********let********
+// reinsilaztion possible but redclartion not possible
+
+// **********const**********
+//both are not possible 
+
+
+// only declartion possible or not
+// var=>possible
+// let => possible
+// const => notpossible
+// let a;
+// console.log(a);
+// const c;
+// console.log(c);
+
+
+// way of declartion and access
+
+// global declartion
+// var -> acess
+
+// local declartion
+
+
+// block access
+// 
+
+
+
