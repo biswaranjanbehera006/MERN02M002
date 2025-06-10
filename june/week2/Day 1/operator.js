86f6891d7fb6661321de3d5bf6ed353ea7c819c8
biswaranjanbehera006 <biswaranjanbehera006@gmail.com>
2025-06-10 13:05:05 +0530

    Loop Tutorial

---
+++ june/week2/Day 1/operator.js
@@ -0,0 +1,77 @@
+// operator
+// Arithmatic Operator
+
+
+let a=5;
+let b=30;
+console.log(a+b); //50
+console.log(a-b); // -10
+console.log(a*b); //600
+console.log(a/b); //0.66
+
+console.log(a%2);//1
+console.log(a**2);//25
+
+console.log(a++); //5
+console.log(++a) //7
+
+console.log(b--) //30
+console.log(--b) //28
+
+console.log("string"+30);
+console.log(20+30+"String")
+console.log(true+"String")
+console.log( +"text");
+console.log(typeof +"text")
+
+console.log(+true)
+console.log("20"+"30")
+console.log("20"+ +"30"*2)
+// console.log("20"++);
+
+let c=40;
+c+=2; //c=c+2
+console.log(c)
+c-=3;
+console.log(c)
+
+// comparision operator
+let st1="xyz";
+let st2="xyz";
+console.log(st1==st2);
+console.log(st1===st2)
+
+let n1="40";
+let n2=20;
+// normal comparision
+console.log(n1==n2)
+// strict comparision
+console.log(n1===n2)
+
+let is="true"
+console.log(true !== is);
+
+
+// let l="2";
+// console.log(l++)
+// console.log(l)
+
+// let f=3;
+// console.log((2>4) && (f++<5))
+// console.log(f)
+
+// console.log(!false)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ june/week2/Day 2/loop.js
@@ -0,0 +1,167 @@
+// // conditional statement
+
+// // let age=prompt("Enter your Age ");
+// let age=23;
+// if(age>=18)
+// {
+//     console.log("He/She can eligible for voating");
+    
+// }
+// else
+// {
+//     console.log("He/She can't eligible for voating");
+    
+// }
+
+// let mark=58;
+
+// if(mark>=90)
+// {
+//     console.log("A");
+    
+// }
+// else if(mark>=70)
+// {
+//     console.log("B");
+    
+// }
+// else if(mark>=30)
+// {
+//     console.log("C");
+    
+// }
+// else
+// {
+//     console.log("Fail");
+    
+// }
+
+
+//Ternary Operator
+//
+// let stage=20;
+// console.log(stage>=18?"Can vote": "can't Vote");
+
+
+// let us=prompt("Enter A number");
+// if(us%5==0)
+// {
+//     console.log("This Number is divisible by 5");
+    
+// }
+// else
+//     {
+//     console.log("This Number is not divisible by 5");
+    
+// }
+
+
+// let even=prompt("Enetre the number to check even or odd");
+// if(even==0)
+// {
+//     console.log(`Zero Not allowed`);
+    
+// }
+// else if(even%2==0)
+// {
+//     console.log(`The Number ${even} is even`);
+    
+// }
+// else
+// {
+//     console.log(`The Number ${even} is odd even`);
+    
+// }
+
+
+//************************====Loops Statement===************************
+
+// 1 for loop
+// 2 while loop
+// 3 do while loop
+
+// for(let i=1;i<=10;i++)
+// {
+//     console.log(i);
+    
+// }
+
+// let i=1;
+// while(i<=7)
+// {
+//     console.log(i);
+//     i++;
+    
+// }
+
+// let n=1;
+// do{
+//     console.log(n);
+//     n++;
+// }while(n<=12);
+
+
+//non prmitivi data type
+
+//===========OBJECT==================
+
+let obj={
+    name:"Biswa",
+    age:22,
+    phone:9337747632,
+    email:"bbehera2024@gift.edu.in"
+}
+
+// console.log(obj.email,obj.phone);
+// console.log(obj["name"]);
+// console.log(obj);
+// obj.name="ranjan";
+// console.log(obj.name);
+
+let arr=[1,2,3,4,5,6]
+
+// console.log(arr[1]);
+// console.log(typeof arr);
+
+
+//====================For In Loop==========================
+
+// it is used for both object and array
+//in object
+// for(const key in obj)
+// {
+//     console.log(obj[key]);
+    
+// }
+
+// in array
+
+// for (let idx in arr)
+// {
+//     console.log(arr[idx]);
+    
+// }
+
+// ====================For of Loop======================
+//ony for array
+
+// for (const e of arr) {
+//     console.log(e); 
+// }
+
+
+let str="Qlith Inovation"
+console.log("length is :",str.length);
+console.log(str[3]);
+
+str[3]="r";
+console.log(str[3]);
+
+
+let c=20;
+console.log(`Value is ${c}`);
+
+
+
+
+
