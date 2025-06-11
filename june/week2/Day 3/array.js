42a2353f0649618fd8989864cfbe0596206c7c7e
biswaranjanbehera006 <biswaranjanbehera006@gmail.com>
2025-06-11 13:07:48 +0530

    Array Lession

---
+++ june/week2/Day 3/array.js
@@ -0,0 +1,124 @@
+// console.log("Hey This is Array Lesson");
+
+// let str="Welcome to Qlith Inovation   ";
+
+// console.log(str.length);
+
+// console.log(str.toUpperCase());
+
+// let ss="BISWA"
+// console.log(ss.toLowerCase());
+
+// console.log("After Trim", str.trim().length);
+// console.log("Here Trim not Applicable",str.length);
+
+
+// console.log(str.charAt(1));
+// console.log(str.concat("Bhubaneswar"));
+
+
+// const newvalue=str.replace("W","D")
+// console.log(newvalue);
+
+
+// console.log(str.slice(0,6)); //it start from the 0 index and end it to the 6-1=5 index
+// console.log(str.slice(2)); // it count form the 2nd index and go to the last index
+// console.log(ss.slice(-2)); // it count from the right side beacuse it is negative
+// console.log(str.slice(4,4));// not happinging
+
+
+// let name=prompt("Enter The Name :");
+// console.log("".concat("@").concat(name).concat(name.length));
+
+// let str="Wellcome"
+// console.log(str.substring(0,3)); //as same as the slice methord but it does not support negative value
+
+
+
+
+
+//=====================ARRAY LESSION======================
+
+// let starr=["babul","sagar","prangya","suchismita","bakadi"];
+// console.log(starr[3]);
+
+// for(let e of starr)
+// {
+//     console.log(e);
+    
+// }
+
+// let numarr=[2,3,4,5,6,8]
+// // let sum=0;
+// // for(let e of numarr)
+// // {
+// //     sum+=e;
+// // }
+// // console.log(sum/(numarr.length));
+
+
+// numarr.push(9);
+// for(let idx of numarr)
+// {
+//     console.log(idx);
+  
+// }
+// numarr.push(0)
+// console.log(numarr);
+
+// numarr.pop()
+// console.log(numarr);
+
+// console.log(numarr.toString());
+
+// let arr2=[12,33,44,23]
+// console.log(numarr.concat(arr2));
+
+// arr2.unshift(0);
+// console.log(arr2);
+
+// arr2.shift()
+// console.log(arr2);
+// console.log(arr2.slice(0,2));
+
+// arr2.splice(1,2,10);
+// console.log(arr2);
+
+
+// arr2.splice(2,1)
+// console.log(arr2);
+
+
+
+// console.log(arr2.includes(12));
+
+// console.log(arr2.reverse());
+
+
+let com=["Bloomberg","Microsoft","uber","google","IBM","netflix"]
+com.shift(0);
+console.log(com);
+
+com.splice(1,1,"Olla");
+console.log(com);
+
+com.push("Amazon")
+console.log(com);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
